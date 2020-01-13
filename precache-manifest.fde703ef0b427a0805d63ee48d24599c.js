d70e6c868de2dac26134ed28901f8de070000a0a
Dominic <dominic_ong@mymail.sutd.edu.sg>
2020-01-14 01:17:08 +0800

    whoops

---
+++ precache-manifest.fde703ef0b427a0805d63ee48d24599c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7ce47a14777e0f43e75f136a0152004",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8c4747f2ea5e5b1c8c3c",
+    "url": "/static/js/2.871f0ed4.chunk.js"
+  },
+  {
+    "revision": "91e81ba571192bfb60fe",
+    "url": "/static/js/main.335829e0.chunk.js"
+  },
+  {
+    "revision": "2cd235b07888bc38fdc8",
+    "url": "/static/js/runtime-main.3b721bfd.js"
+  }
+]);
--- src/utill/ObjectUtil.ts
+++
@@ -1,3 +0,0 @@
-export function ObjectMap<T, U>(obj: { [key: string]: T }, func: (key: string, val: T, index: number) => U): U[] {
-    return Object.keys(obj).map((k, i) => func(k, obj[k], i));
-}
